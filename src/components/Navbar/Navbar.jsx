aa6c9dae6906e8db4be0d516d944727d3addfa9b
Rafael Vaz <eurafavaz@gmail.com>
2022-05-25 12:47:22 -0300

    class-two-react-project-two

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,11 @@
-import PaletaLista from "./PaletaLista";
-import "./Home.css";
-import sacola from "../assets/icons/sacola.svg";
-import logo from "../assets/logo.svg";
+import './Navbar.css'
 
-function Home() {
-  return (
-    <div className="Home">
-      <div className="Home__header Header">
+import sacola from "assets/icons/sacola.svg";
+import logo from "assets/logo.svg";
+
+function Navbar(){
+    return (
+        <div className="Home__header Header">
         <div className="row">
           <div className="Header__logo Logo">
             <img
@@ -29,12 +28,7 @@ function Home() {
           </div>
         </div>
       </div>
-
-      <div className="Home__container">
-        <PaletaLista />
-      </div>
-    </div>
-  );
+    );
 }
 
-export default Home;
+export default Navbar;
